4a7ad428a54a5aba0196e1db05264af0657e1302
Chris Chrysostom <chris.chrysostom@gmail.com>
2016-12-06 22:33:34 -0700

    Review and logging.

--- payfwd.go
+++ payfwd.go
@@ -4,8 +4,11 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -25,7 +28,39 @@ type BitcoinAddress struct {
 	Sent     int    `json:"total_sent"`
 }
 
+var (
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func Init(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
 func main() {
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
 	db, err := sql.Open("sqlite3", "/home/chris/Documents/Alexandria/payproc/payproc.db")
 	if err != nil {
 		log.Fatal(err)
@@ -57,25 +92,25 @@ func main() {
 	}
 
 	var getCount int
-	fmt.Println("Number of payment addresses", len(pymtToDest))
+	Info.Println("Number of payment addresses", len(pymtToDest))
 
 	addrBalExistsStmt, stmtErr := db.Prepare("SELECT paymentAddress, balance, balanceDate FROM AddressBalance WHERE paymentAddress = ?")
 	if stmtErr != nil {
-		log.Fatal(stmtErr)
+		Error.Println(stmtErr)
 		return
 	}
 	defer addrBalExistsStmt.Close()
 
 	insertBalStmt, insErr := db.Prepare("INSERT INTO AddressBalance (paymentAddress, balance, balanceDate) VALUES (?, ?, datetime('now'))")
 	if insErr != nil {
-		log.Fatal(insErr)
+		Error.Println(insErr)
 		return
 	}
 	defer insertBalStmt.Close()
 
 	updateBalStmt, updErr := db.Prepare("UPDATE AddressBalance SET balance = ?, balance = datetime('now') WHERE paymentAddress = ?")
 	if updErr != nil {
-		log.Fatal(updErr)
+		Error.Println(updErr)
 		return
 	}
 	defer updateBalStmt.Close()
@@ -85,29 +120,32 @@ func main() {
 
 		getCount++
 		if err != nil {
-			log.Fatal(err)
+			Error.Println(err)
+			break
 		}
 
 		if resp.StatusCode != 200 {
 			fmt.Println("Status Code", resp.StatusCode)
 			fmt.Println("Status", resp.Status)
-			break
+			continue
 		}
 
 		addressInfo := new(BitcoinAddress)
 		readErr := json.NewDecoder(resp.Body).Decode(addressInfo)
 		if readErr != nil {
-			log.Fatal(readErr)
+			Error.Println(readErr)
+			resp.Body.Close()
+			continue
 		}
 		resp.Body.Close()
 
 		if addressInfo.Balance > 0 {
-			fmt.Println("GET Count", getCount, "Payment address", addressInfo.Address, "Balance", addressInfo.Balance, "Total Received", addressInfo.Received, "Payable", value.PayableBalance)
+			Info.Println("GET Count", getCount, "Payment address", addressInfo.Address, "Balance", addressInfo.Balance, "Total Received", addressInfo.Received, "Payable", value.PayableBalance)
 		}
 
 		existsRows, existsErr := addrBalExistsStmt.Query(addressInfo.Address)
 		if existsErr != nil {
-			log.Fatal(existsErr)
+			Error.Println(existsErr)
 			break
 		}
 
@@ -117,27 +155,29 @@ func main() {
 		}
 
 		if existsCount > 0 {
+			Info.Println("Updating address", addressInfo.Address, "balance", addressInfo.Balance)
 			updResult, updateErr := updateBalStmt.Exec(addressInfo.Address, addressInfo.Balance)
 			if updateErr != nil {
-				log.Fatal(updateErr)
+				Error.Println(updateErr)
 				break
 			}
 			rowsUpdated, _ := updResult.RowsAffected()
 			if rowsUpdated < 1 {
-				log.Fatal(fmt.Sprintf("Failed to update AddressBalance for address, %s.", addressInfo.Address))
+				Error.Println(fmt.Sprintf("Failed to update AddressBalance for address, %s.", addressInfo.Address))
 			}
 		} else {
+			Info.Println("Inserting new address", addressInfo.Address, "balance", addressInfo.Balance)
 			insResult, insErr := insertBalStmt.Exec(addressInfo.Address, addressInfo.Balance)
 			if insErr != nil {
-				log.Fatal(insErr)
+				Error.Println(insErr)
 				break
 			}
 			rowsInserted, _ := insResult.RowsAffected()
 			if rowsInserted < 1 {
-				log.Fatal(fmt.Sprintf("Failed to insert AddressBalance for address, %s", addressInfo.Address))
+				Error.Println(fmt.Sprintf("Failed to insert AddressBalance for address, %s", addressInfo.Address))
 			}
 		}
 
 	}
-	fmt.Println("Total address count:", getCount)
+	Info.Println("Total address count:", getCount)
 }
